97498e31a81454d37b445340476d47cc3661d6d9
Aayobami001 <aibrahim.akinbiyi@gmail.com>
2024-05-02 02:41:54 -0700

    added comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user fir their name.
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
