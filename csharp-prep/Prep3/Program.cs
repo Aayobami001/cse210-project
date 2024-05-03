e0548ac7c9c8b8352bf01de4fa7f1ccf3c6fb034
Aayobami001 <aibrahim.akinbiyi@gmail.com>
2024-05-03 08:54:00 -0700

    Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+         // Generate a random magic number between 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        // Initialize guess to a value outside the range of magicNumber to ensure the loop starts
+        int guess = -1;
+
+        // Loop until the user guesses the magic number
+        while (guess != magicNumber)
+        {
+            // Ask user for a guess
+            Console.Write("Make a guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            // Check if the guess is correct
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("Congratulations! You guessed it right!");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Try guessing higher next time.");
+            }
+            else
+            {
+                Console.WriteLine("Try guessing lower next time.");
+            }
+        }
     }
 }
